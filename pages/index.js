723b678c2386736d960dd70f26aac0a1cdff4521
Gerard Hynes <gkhynes@gmail.com>
2021-06-29 11:36:44 +0100

    List properties on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-import { sanityClient } from "../sanity";
+import { sanityClient, urlFor } from "../sanity";
+import Link from "next/link";
+import { isMultiple } from "../utils";
 
 export default function Home({ properties }) {
-  return <h1>Óstach</h1>;
+  return (
+    <>
+      <h1>Óstach</h1>
+      <div className="main">
+        <div className="feed-container">
+          <h2>Places to stay near you</h2>
+          <div className="feed">
+            {properties.map((property) => (
+              <Link href={`property/${property.slug.current}`}>
+                <div key={property._id} className="card">
+                  <img src={urlFor(property.mainImage)} alt={property.name} />
+                  <p>
+                    {property.reviews.length} review
+                    {isMultiple(property.reviews.length)}
+                  </p>
+                  <h3>{property.title}</h3>
+                  <p>€{property.pricePerNight} per night</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+          <div className="map"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export async function getServerSideProps() {
